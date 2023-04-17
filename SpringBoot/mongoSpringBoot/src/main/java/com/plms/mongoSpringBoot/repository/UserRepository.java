455ff7fd3446f79c1362cb5227606713fe474cea
PabloLms <pablolomusan@hotmail.com>
2023-04-16 22:45:42 -0500

    add queries mongoDb

--- SpringBoot/mongoSpringBoot/src/main/java/com/plms/mongoSpringBoot/repository/UserRepository.java
+++ SpringBoot/mongoSpringBoot/src/main/java/com/plms/mongoSpringBoot/repository/UserRepository.java
@@ -1,10 +1,21 @@
 package com.plms.mongoSpringBoot.repository;
 
-import com.plms.mongoSpringBoot.dto.User;
+import com.plms.mongoSpringBoot.entities.User;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
+  Optional<User> findFirstByLastName(final String lastName);
+
+  List<User> findByLastName(final String lastName);
+
+  Long deleteByAge(final Integer age);
+
+  Long deleteByAgeAndLastName(final Integer age, final String lastName);
+
+  Long deleteByAgeAndLastNameAndName(final Integer age, final String lastName, final String name);
 }
