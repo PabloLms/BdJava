2f858adb2d8ce72d94efa262564a47b5ad776bad
PabloLms <pablolomusan@hotmail.com>
2023-04-16 22:37:38 -0500

    refactore code in endPoints

--- SpringBoot/mongoSpringBoot/src/main/java/com/plms/mongoSpringBoot/controller/UserController.java
+++ SpringBoot/mongoSpringBoot/src/main/java/com/plms/mongoSpringBoot/controller/UserController.java
@@ -1,20 +1,24 @@
 package com.plms.mongoSpringBoot.controller;
 
-import com.plms.mongoSpringBoot.dto.User;
 import com.plms.mongoSpringBoot.dto.UserDto;
+import com.plms.mongoSpringBoot.entities.User;
 import com.plms.mongoSpringBoot.handler.ResponseHandle;
 import com.plms.mongoSpringBoot.mapper.UserMapper;
 import com.plms.mongoSpringBoot.repository.UserRepository;
 import java.util.List;
 import java.util.Optional;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,75 +31,113 @@ public class UserController extends AbstractMongoController<User, String> {
   @Autowired
   private UserMapper userMapper;
 
-  @Autowired
-  private MongoTemplate mongoTemplate;
+//  @Autowired
+//  private MongoTemplate mongoTemplate;
 
 
   @RequestMapping(method = RequestMethod.GET)
   @Override
   public ResponseEntity<Object> getAll() {
     try {
-      List<User> users = mongoTemplate.findAll(User.class);
-      return ResponseHandle.generateResponse("Success", HttpStatus.OK, users);
-    } catch (Exception ex) {
-      return ResponseHandle.generateResponse(ex.getMessage(), HttpStatus.MULTI_STATUS, null);
+      final List<User> users = this.userRepository.findAll();
+      return ResponseHandle.generateResponseGet(users);
+    } catch (final Exception ex) {
+      return ResponseHandle.generateResponseException(ex.getMessage());
     }
+  }
+
+  @RequestMapping(method = RequestMethod.GET, value = "/paginate")
+  public ResponseEntity<Page<User>> getPaginate(
+      @RequestParam(required = false, name = "index") final Integer optionalIndex,
+      @RequestParam(required = false, name = "max") final Integer optionalMax) {
+    final int index = Optional.ofNullable(optionalIndex).orElse(0);
+    final int max = Optional.ofNullable(optionalMax).orElse(2);
+    final HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.set("indexDefault", "0");
+    httpHeaders.set("maxDefault", "2");
+    return ResponseEntity.ok().headers(httpHeaders)
+        .body(this.userRepository.findAll(PageRequest.of(index, max)));
+  }
+
+  @RequestMapping(method = RequestMethod.GET, value = "/paginate/content")
+  public List<User> getPaginateContent(
+      @RequestParam(required = false, name = "index") final Integer optionalIndex,
+      @RequestParam(required = false, name = "max") final Integer optionalMax) {
+    final int index = Optional.ofNullable(optionalIndex).orElse(0);
+    final int max = Optional.ofNullable(optionalMax).orElse(2);
+    return this.userRepository.findAll(PageRequest.of(index, max)).getContent();
   }
 
   @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
   @Override
-  public ResponseEntity<Object> get(@PathVariable String userId) {
+  public ResponseEntity<Object> get(@PathVariable final String userId) {
     try {
-      Optional<User> userOptional = userRepository.findById(userId);
-      return userOptional.map(
-          user -> ResponseHandle.generateResponse("Success", HttpStatus.OK, user)).orElseGet(
-          () -> ResponseHandle.generateResponse("Not found user", HttpStatus.NOT_FOUND, null));
-    } catch (Exception ex) {
-      return ResponseHandle.generateResponse(ex.getMessage(), HttpStatus.MULTI_STATUS, null);
+      final Optional<User> userOptional = this.userRepository.findById(userId);
+      return userOptional.map(ResponseHandle::generateResponseGet)
+          .orElseGet(ResponseHandle::generateResponseNotFound);
+    } catch (final Exception ex) {
+      return ResponseHandle.generateResponseException(ex.getMessage());
+    }
+  }
+
+  @RequestMapping(value = "/findLastName", method = RequestMethod.GET)
+  public ResponseEntity<Object> getLastName(@RequestParam final String lastName) {
+    try {
+      final Optional<User> userOptional = this.userRepository.findFirstByLastName(lastName);
+      return userOptional.map(ResponseHandle::generateResponseGet)
+          .orElseGet(ResponseHandle::generateResponseNotFound);
+    } catch (final Exception ex) {
+      return ResponseHandle.generateResponseException(ex.getMessage());
     }
   }
 
   @RequestMapping(value = "/create", method = RequestMethod.POST)
   @Override
-  public ResponseEntity<Object> create(@RequestBody User user) {
+  public ResponseEntity<Object> create(@RequestBody final User user) {
     try {
-      User userSave = userRepository.save(user);
-      return ResponseHandle.generateResponse("user created", HttpStatus.OK, userSave);
-    } catch (Exception ex) {
-      return ResponseHandle.generateResponse(ex.getMessage(), HttpStatus.MULTI_STATUS, null);
+      final User userSave = this.userRepository.save(user);
+      return ResponseHandle.generateResponseCreate(userSave);
+    } catch (final Exception ex) {
+      return ResponseHandle.generateResponseException(ex.getMessage());
     }
   }
 
   @RequestMapping(value = "/update/{userId}", method = RequestMethod.PUT)
 //  @Override
-  public ResponseEntity<Object> update(@RequestBody UserDto user, @PathVariable String userId) {
+  public ResponseEntity<Object> update(@RequestBody final UserDto userDto,
+      @PathVariable final String userId) {
     try {
-      Optional<User> optionalUser = userRepository.findById(userId);
-      if (optionalUser.isPresent()) {
-        User userUpdate = optionalUser.get();
-
-//        userUpdate.setName(user.getName());
-//        userUpdate.setLastName(user.getLastName());
-//        userUpdate.setAge(user.getAge());
-        userMapper.update(user, userUpdate);
-        User userSave = userRepository.save(userUpdate);
-        return ResponseHandle.generateResponse("user updated", HttpStatus.OK, userSave);
-      } else {
-        return ResponseHandle.generateResponse("user not found", HttpStatus.MULTI_STATUS, null);
-      }
-    } catch (Exception ex) {
-      return ResponseHandle.generateResponse(ex.getMessage(), HttpStatus.MULTI_STATUS, null);
+      final Optional<User> optionalUser = this.userRepository.findById(userId);
+      return optionalUser.map(user -> {
+        this.userMapper.update(userDto, user);
+        final User userSave = this.userRepository.save(user);
+        return ResponseHandle.generateResponseUpdate(userSave);
+      }).orElseGet(ResponseHandle::generateResponseNotFound);
+    } catch (final Exception ex) {
+      return ResponseHandle.generateResponseException(ex.getMessage());
     }
   }
 
   @RequestMapping(value = "/delete/{userId}", method = RequestMethod.DELETE)
   @Override
-  public ResponseEntity<Object> delete(@PathVariable String userId) {
+  public ResponseEntity<Object> delete(@PathVariable final String userId) {
     try {
-      userRepository.deleteById(userId);
-      return ResponseHandle.generateResponse("Delete SuccessFully", HttpStatus.OK, null);
-    } catch (Exception ex) {
-      return ResponseHandle.generateResponse(ex.getMessage(), HttpStatus.MULTI_STATUS, null);
+      this.userRepository.deleteById(userId);
+      return ResponseHandle.generateResponseDelete(User.class);
+    } catch (final Exception ex) {
+      return ResponseHandle.generateResponseException(ex.getMessage());
+    }
+  }
+
+  @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+  public ResponseEntity<Object> deleteParams(@RequestParam(name = "age") final Integer age) {
+    try {
+      final Long countUsers = this.userRepository.deleteByAge(age);
+      return ResponseHandle.generateResponse(
+          StringUtils.join("Numbers deleted of users: ", countUsers),
+          HttpStatus.OK, null);
+    } catch (final Exception ex) {
+      return ResponseHandle.generateResponseException(ex.getMessage());
     }
   }
 }
--- SpringBoot/mongoSpringBoot/src/main/java/com/plms/mongoSpringBoot/handler/ResponseHandle.java
+++ SpringBoot/mongoSpringBoot/src/main/java/com/plms/mongoSpringBoot/handler/ResponseHandle.java
@@ -2,6 +2,7 @@ package com.plms.mongoSpringBoot.handler;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -10,14 +11,50 @@ public class ResponseHandle {
   public static final String MESSAGE = "message";
   public static final String STATUS = "status";
   public static final String DATA = "data";
+  public static final String MESSAGE_GET = "Success";
+  public static final String MESSAGE_CREATED = " created successfully";
 
-  public static ResponseEntity<Object> generateResponse(String message, HttpStatus httpStatus,
-      Object object) {
+  public static final String MESSAGE_UPDATED = " updated successfully";
+
+  public static final String MESSAGE_NOT_FOUND = "Not found";
+
+  private static final String MESSAGE_DELETE = " deleted successfully";
+
+  public static ResponseEntity<Object> generateResponse(final String message,
+      final HttpStatus httpStatus, final Object object) {
     final Map<String, Object> map = new HashMap<>();
     map.put(MESSAGE, message);
     map.put(STATUS, httpStatus.value());
     map.put(DATA, object);
     return new ResponseEntity<>(map, httpStatus);
   }
+
+  public static ResponseEntity<Object> generateResponseGet(final Object object) {
+    return generateResponse(MESSAGE_GET, HttpStatus.OK, object);
+  }
+
+  public static ResponseEntity<Object> generateResponseCreate(final Object object) {
+    final String message = StringUtils.join(object.getClass().getSimpleName(), MESSAGE_CREATED);
+    return generateResponse(message, HttpStatus.OK, object);
+  }
+
+  public static ResponseEntity<Object> generateResponseUpdate(final Object object) {
+    final String message = StringUtils.join(object.getClass().getSimpleName(), MESSAGE_UPDATED);
+    return generateResponse(message, HttpStatus.OK, object);
+  }
+
+  public static ResponseEntity<Object> generateResponseDelete(final Class<?> clazz) {
+    final String message = StringUtils.join(clazz.getSimpleName(), MESSAGE_DELETE);
+    return generateResponse(message, HttpStatus.OK, null);
+  }
+
+  public static ResponseEntity<Object> generateResponseNotFound() {
+    return generateResponse(MESSAGE_NOT_FOUND, HttpStatus.NOT_FOUND, null);
+  }
+
+  public static ResponseEntity<Object> generateResponseException(final String message) {
+    return generateResponse(message, HttpStatus.INTERNAL_SERVER_ERROR, null);
+  }
+
 
 }
